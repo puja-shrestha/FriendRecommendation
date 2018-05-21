977d44f14d8b6bfbe837cb50ab93d55525d96fbb
Luckey <luckeysherpa07@gmail.com>
2018-05-21 19:41:08 +0545

    Added another text

--- AppTabNavigation/ChatTab.js
+++ AppTabNavigation/ChatTab.js
@@ -23,6 +23,7 @@ class ChatTab extends Component {
             
             <Text>Puja Shrestha</Text>
             <Text>Luckey Sherpa</Text>
+            <Text>Thinley Sherpa</Text>
           </View>
         );
     }
